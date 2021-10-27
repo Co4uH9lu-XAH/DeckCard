c9bdf4c4fad9f3d7958741445a407dd1b77bf00c
Co4uH9lu-XAH <eugenkunin@gmail.com>
2021-10-27 16:20:47 +0300

    Me vs Random method@

--- src/GiveMeCard.java
+++ src/GiveMeCard.java
@@ -5,7 +5,8 @@ public class GiveMeCard {
     public static void main(String[] args) {
         ArrayList<Card> deck = SaveDeckOfCards.saveDeck();
         ArrayList<Card> newDeck = new ArrayList<>();
-        for (int i = 0; i<10; i++)
-        System.out.println(newDeck.add(RandomCard.getCard(deck)).toString());
+        for (int i = 0; i<36; i++)
+        newDeck.add(RandomCard.getCard(deck));
+        newDeck.toString();
     }
 }
--- src/RandomCard.java
+++ src/RandomCard.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RandomCard {
 
@@ -6,9 +7,12 @@ public class RandomCard {
     private static Object Card;
 
     public static Card getCard (ArrayList<Card> deck) {
+        Random generator = new Random();
+        int r = generator.nextInt(deck.size());
         int n = (int)Math.floor(Math.random() * deck.size());
         ArrayList<Card> newDeck = new ArrayList<>();
-        newDeck.add(deck.get(n));
+        newDeck.add(deck.get(r));
+        //deck.remove(n);
         return newDeck.get(0);
     }
 }
